5a03173e710fb26e8610950af4779a4a7ce6a788
heartconnect-1 <heartconnec1@gmail.com>
2026-09-08 01:02:56 +0300

    Sync src/Stage5Experience.tsx from AppDeploy v18

---
+++ src/Stage5Experience.tsx
@@ -0,0 +1,5 @@
+import Stage4Experience from './Stage4Experience';
+import './stage5.css';
+import './stage5-finish.css';
+type Props=React.ComponentProps<typeof Stage4Experience>;
+export default function Stage5Experience(props:Props){return <Stage4Experience {...props}/>}
