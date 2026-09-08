33143edcff8424a2666cc1723ac57ba0ea401f90
heartconnect-1 <heartconnec1@gmail.com>
2026-09-08 23:26:21 +0300

    Route public URLs to dedicated public site

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import PublicSite,{isPublicPath} from './PublicSite';
 import { startSupabaseBridge } from './lib/supabase-bridge';
 import './index.css';
 
-if('serviceWorker' in navigator)window.addEventListener('load',()=>navigator.serviceWorker.register('./sw.js').catch(()=>{}));
+if('serviceWorker' in navigator)window.addEventListener('load',()=>navigator.serviceWorker.register('/sw.js').catch(()=>{}));
 startSupabaseBridge();
 
+const Root=isPublicPath(window.location.pathname)?PublicSite:App;
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <Root />
     </StrictMode>
 );
