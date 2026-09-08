0d7c88707e30f5fffc61e4f7b0d4a4458f015af2
heartconnect-1 <heartconnec1@gmail.com>
2026-09-09 00:19:44 +0300

    Wire public routes and 404 handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import PublicSite,{isPublicPath} from './PublicSite';
 import { startSupabaseBridge } from './lib/supabase-bridge';
 import './index.css';
 
-if('serviceWorker' in navigator)window.addEventListener('load',()=>navigator.serviceWorker.register('./sw.js').catch(()=>{}));
+if('serviceWorker' in navigator)window.addEventListener('load',()=>navigator.serviceWorker.register('/sw.js').catch(()=>{}));
 startSupabaseBridge();
+
+const pathname=window.location.pathname;
+const Root=isPublicPath(pathname)?PublicSite:App;
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <Root />
     </StrictMode>
 );
