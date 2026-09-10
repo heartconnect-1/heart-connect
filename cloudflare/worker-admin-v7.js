94583ff7085c37a26f598bed8a2d088f5c6318ab
heartconnect-1 <heartconnec1@gmail.com>
2026-09-10 22:45:47 +0300

    admin: route advanced payment operations v29

--- cloudflare/worker-admin-v7.js
+++ cloudflare/worker-admin-v7.js
@@ -1,9 +1,9 @@
 import previousWorker from './worker-v15.js';
-import {handleAdmin,runPhase7AndEarlierScheduled,VERSION as ADMIN_VERSION} from './admin-api-v28.js';
+import {handleAdmin,runPhase7AndEarlierScheduled,VERSION as ADMIN_VERSION} from './admin-api-v29.js';
 import {handleAdminLogin} from './admin-login-v2.js';
 import BOOKING_ADVANCED from './booking-advanced-v10.txt';
 
-const EDGE_VERSION='cloudflare-admin-router-v29-verification';
+const EDGE_VERSION='cloudflare-admin-router-v30-payments';
 const BOOKING_ASSET='/_hc/booking-advanced-v10.js';
 const BOOKING_LOCATION_SEARCH='/_hc/booking-location-search';
 
