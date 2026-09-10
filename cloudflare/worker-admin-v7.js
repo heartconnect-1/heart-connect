1a96936bab9e70adb292e55bdd5af7ec69a990d1
heartconnect-1 <heartconnec1@gmail.com>
2026-09-10 14:44:15 +0300

    admin: route integration preview through v8 staff management

--- cloudflare/worker-admin-v7.js
+++ cloudflare/worker-admin-v7.js
@@ -1,8 +1,8 @@
 import previousWorker from './worker-v15.js';
-import {handleAdmin,runPhase7AndEarlierScheduled,VERSION as ADMIN_VERSION} from './admin-api-v7.js';
+import {handleAdmin,runPhase7AndEarlierScheduled,VERSION as ADMIN_VERSION} from './admin-api-v8.js';
 import {handleAdminLogin} from './admin-login-v1.js';
 
-const EDGE_VERSION='cloudflare-admin-router-v7-integration';
+const EDGE_VERSION='cloudflare-admin-router-v8-integration';
 function stamp(response){const h=new Headers(response.headers);h.set('x-heart-connect-admin-router',EDGE_VERSION);h.set('x-heart-connect-admin-version',ADMIN_VERSION);h.set('x-heart-connect-security-core','cloudflare-router-v15');return new Response(response.body,{status:response.status,statusText:response.statusText,headers:h})}
 function hasNativeSessionCookie(request){return /(?:^|;\s*)__Host-hc_cf_access=/.test(String(request.headers.get('cookie')||''))}
 
