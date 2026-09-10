d6d3e4b63a329be5be57e753bc2f8a7ec6f187ca
heartconnect-1 <heartconnec1@gmail.com>
2026-09-10 15:14:10 +0300

    admin: use polished secure sign-in screen

--- cloudflare/worker-admin-v7.js
+++ cloudflare/worker-admin-v7.js
@@ -1,6 +1,6 @@
 import previousWorker from './worker-v15.js';
 import {handleAdmin,runPhase7AndEarlierScheduled,VERSION as ADMIN_VERSION} from './admin-api-v9.js';
-import {handleAdminLogin} from './admin-login-v1.js';
+import {handleAdminLogin} from './admin-login-v2.js';
 
 const EDGE_VERSION='cloudflare-admin-router-v9-final-polish';
 function stamp(response){const h=new Headers(response.headers);h.set('x-heart-connect-admin-router',EDGE_VERSION);h.set('x-heart-connect-admin-version',ADMIN_VERSION);h.set('x-heart-connect-security-core','cloudflare-router-v15');h.set('x-content-type-options','nosniff');h.set('referrer-policy','no-referrer');h.set('permissions-policy','camera=(), microphone=(), geolocation=(), payment=(), usb=(), serial=()');h.set('strict-transport-security','max-age=31536000; includeSubDomains');return new Response(response.body,{status:response.status,statusText:response.statusText,headers:h})}
