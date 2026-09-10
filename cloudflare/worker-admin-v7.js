827453060491f44288f24760948c108c46c7c6f3
heartconnect-1 <heartconnec1@gmail.com>
2026-09-11 01:58:26 +0300

    feat(admin): activate Reports and Search v44

--- cloudflare/worker-admin-v7.js
+++ cloudflare/worker-admin-v7.js
@@ -1,9 +1,9 @@
 import previousWorker from './worker-v15.js';
-import {handleAdmin,runPhase7AndEarlierScheduled,VERSION as ADMIN_VERSION} from './admin-api-v43.js';
+import {handleAdmin,runPhase7AndEarlierScheduled,VERSION as ADMIN_VERSION} from './admin-api-v44.js';
 import {handleAdminLogin} from './admin-login-v2.js';
 import BOOKING_ADVANCED from './booking-advanced-v10.txt';
 
-const EDGE_VERSION='cloudflare-admin-router-v43-current-command-center';
+const EDGE_VERSION='cloudflare-admin-router-v44-reports-search-intelligence';
 const BOOKING_ASSET='/_hc/booking-advanced-v10.js';
 const BOOKING_LOCATION_SEARCH='/_hc/booking-location-search';
 
