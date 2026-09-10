4d4a0a492f5b01967c8abf78ba514b786407b2ef
heartconnect-1 <heartconnec1@gmail.com>
2026-09-11 02:28:31 +0300

    fix(admin): activate cache-safe Staff v47

--- cloudflare/worker-admin-v7.js
+++ cloudflare/worker-admin-v7.js
@@ -1,9 +1,9 @@
 import previousWorker from './worker-v15.js';
-import {handleAdmin,runPhase7AndEarlierScheduled,VERSION as ADMIN_VERSION} from './admin-api-v46.js';
+import {handleAdmin,runPhase7AndEarlierScheduled,VERSION as ADMIN_VERSION} from './admin-api-v47.js';
 import {handleAdminLogin} from './admin-login-v2.js';
 import BOOKING_ADVANCED from './booking-advanced-v10.txt';
 
-const EDGE_VERSION='cloudflare-admin-router-v46-direct-staff-photo-upload';
+const EDGE_VERSION='cloudflare-admin-router-v47-cache-safe-staff-render';
 const BOOKING_ASSET='/_hc/booking-advanced-v10.js';
 const BOOKING_LOCATION_SEARCH='/_hc/booking-location-search';
 
