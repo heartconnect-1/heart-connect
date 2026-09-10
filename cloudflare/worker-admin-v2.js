e87d14fa1d08f9873296019426e646dd278b1dcb
heartconnect-1 <heartconnec1@gmail.com>
2026-09-10 11:14:24 +0300

    Route isolated admin preview through phase 2 control plane

---
+++ cloudflare/worker-admin-v2.js
@@ -0,0 +1,13 @@
+import previousWorker from './worker-v12.js';
+import {handleAdmin,VERSION as ADMIN_VERSION} from './admin-api-v2.js';
+
+const EDGE_VERSION='cloudflare-admin-router-v2';
+function stamp(response){const h=new Headers(response.headers);h.set('x-heart-connect-admin-router',EDGE_VERSION);h.set('x-heart-connect-admin-version',ADMIN_VERSION);return new Response(response.body,{status:response.status,statusText:response.statusText,headers:h})}
+
+export default {
+  async fetch(request,env,ctx){
+    const admin=await handleAdmin(request,env);
+    if(admin)return stamp(admin);
+    return previousWorker.fetch(request,env,ctx);
+  }
+};
