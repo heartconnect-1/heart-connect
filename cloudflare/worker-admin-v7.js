ad6999438e3d94c68b5e1f3dfa7afdebe08eefc1
heartconnect-1 <heartconnec1@gmail.com>
2026-09-10 23:42:03 +0300

    admin: route page media upload v34

--- cloudflare/worker-admin-v7.js
+++ cloudflare/worker-admin-v7.js
@@ -1,9 +1,9 @@
 import previousWorker from './worker-v15.js';
-import {handleAdmin,runPhase7AndEarlierScheduled,VERSION as ADMIN_VERSION} from './admin-api-v33.js';
+import {handleAdmin,runPhase7AndEarlierScheduled,VERSION as ADMIN_VERSION} from './admin-api-v34.js';
 import {handleAdminLogin} from './admin-login-v2.js';
 import BOOKING_ADVANCED from './booking-advanced-v10.txt';
 
-const EDGE_VERSION='cloudflare-admin-router-v34-pages';
+const EDGE_VERSION='cloudflare-admin-router-v35-page-upload';
 const BOOKING_ASSET='/_hc/booking-advanced-v10.js';
 const BOOKING_LOCATION_SEARCH='/_hc/booking-location-search';
 
